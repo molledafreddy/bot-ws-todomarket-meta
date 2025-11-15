d42000aa08b5c25ee9f2b491a0082a67a29c4039
Freddy Molleda <freddymolleda@MacBook-Pro-de-Freddy.local>
2025-11-15 11:13:20 -0300

    datos

--- src/app.ts
+++ src/app.ts
@@ -1287,7 +1287,7 @@ const main = async () => {
         jwtToken: process.env.JWT_TOKEN!,
         numberId: process.env.NUMBER_ID!,
         verifyToken: process.env.VERIFY_TOKEN!,
-        version: 'v22.0'
+        version: 'v23.0'
     })
 
     // const adapterProvider = createProvider(Provider, {
